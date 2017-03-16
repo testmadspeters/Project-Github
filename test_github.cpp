4a6877f186ed44270c0287f905323372968c0566
madspeters <madsp16@student.sdu.dk>
2017-03-16 17:40:05 +0100

    Testcommit

---
+++ test_github.cpp
@@ -0,0 +1,15 @@
+//
+//  test_github.cpp
+//  
+//
+//  Created by Mads Peters on 16/03/2017.
+//
+//
+
+#include "test_github.hpp"
+#include <iostream>
+
+int main() {
+    cout << "Dette er en testfil" << endl;
+    return 0;
+}
---
+++ test_github.hpp
@@ -0,0 +1,14 @@
+//
+//  test_github.hpp
+//  
+//
+//  Created by Mads Peters on 16/03/2017.
+//
+//
+
+#ifndef test_github_hpp
+#define test_github_hpp
+
+#include <stdio.h>
+
+#endif /* test_github_hpp */
